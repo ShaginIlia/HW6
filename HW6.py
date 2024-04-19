5c1fdd2bc9177e635d2aa7fee5ee5fe4ee072793
ShaginIlia <166448106+ShaginIlia@users.noreply.github.com>
2024-04-20 00:38:12 +0300

    Add files via upload

---
+++ HW6.py
@@ -0,0 +1,18 @@
+# Задание по спискам
+
+my_list = ['apple','banane','pear','pineapple','strawberry']
+print(my_list)
+print(my_list[0])
+print(my_list[-1])
+print(my_list[2:])
+my_list[2] = 'ice'
+print(my_list)
+
+# Задание по словарям
+
+my_dict = {'apple': 'яблоко','banane': 'банан','pear': 'груша','pineapple': 'ананас','strawberry': 'клубника'}
+print(my_dict)
+print(my_dict['pear'])
+my_dict['banane'] = 'миньон'
+my_dict['kiwi'] = ('киви')
+print(my_dict)
